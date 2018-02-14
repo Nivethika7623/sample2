b19b2105d61422fdbf344159b01d89d5832ea097
Nivethika7623 <36299119+Nivethika7623@users.noreply.github.com>
2018-02-14 15:15:10 +0530

    posornega.py
    check whether a number is positive or negative  or zero.
    input size:1<=N<=100000

---
+++ posornega.py
@@ -0,0 +1,9 @@
+N=float(input("Enter the number:"))
+if(1<=N<=100000):
+print("The given number is positive")
+elif(N==0):
+print("The given number is zero")
+elif(N<0):
+print("The given number is negative")
+elif(N>100000):
+print("The given number is invalid")
