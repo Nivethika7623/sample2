088c6c62e92690b6e0acee68d794f41651d3bb67
Nivethika7623 <36299119+Nivethika7623@users.noreply.github.com>
2018-03-14 17:21:31 +0530

    Update b32.py

--- b32.py
+++ b32.py
@@ -1,11 +1,11 @@
-string=str(input("Enter string:"))
-char=0
-word=1
-for i in string:
-      char=char+1
+str1=str(input("Enter string:"))
+char1=0
+word1=1
+for i in str1:
+      char1=char1+1
       if(i==' '):
-            word=word+1
+            word1=word1+1
 print("Number of words in the string:")
-print(word)
+print(word1)
 print("Number of characters in the string:")
-print(char)
+print(char1)
