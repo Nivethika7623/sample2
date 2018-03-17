9d5e6409d4bde4dad481f7ec38c37dcfba5f47c6
Nivethika7623 <36299119+Nivethika7623@users.noreply.github.com>
2018-03-17 10:42:39 +0530

    Update b47.py

--- b47.py
+++ b47.py
@@ -1,8 +1,8 @@
-a=[]
-n=int(input("Enter number of elements:"))
-for i in range(1,n+1):
-    b=int(input("Enter element:"))
-    a.append(b)
-a.sort()
-print(min(a))
-print(max(a))
+a1=[]
+n1=int(input("Enter number of elements:"))
+for i in range(1,n1+1):
+    b1=int(input("Enter element:"))
+    a1.append(b1)
+a1.sort()
+print(min(a1))
+print(max(a1))
