7b7d963d48b22357215bfebfabcf182228315814
Nivethika7623 <36299119+Nivethika7623@users.noreply.github.com>
2018-03-19 16:05:32 +0530

    Update b57.py

--- b57.py
+++ b57.py
@@ -1,7 +1,7 @@
-x = input('Enter value of x: ')
-y = input('Enter value of y: ')
-temp = x
-x = y
-y = temp
-print('The value of x after swapping: {}'.format(x))
-print('The value of y after swapping: {}'.format(y))
+x1 = input('Enter value of x1: ')
+y1 = input('Enter value of y1: ')
+temp = x1
+x1 = y1
+y1 = temp
+print('The value of x after swapping: {}'.format(x1))
+print('The value of y after swapping: {}'.format(y1))
