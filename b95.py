86289f69267ce52cbb375c9704ada4cd5400b022
Nivethika7623 <36299119+Nivethika7623@users.noreply.github.com>
2018-03-22 15:11:04 +0530

    b95.py

---
+++ b95.py
@@ -0,0 +1,5 @@
+principle1=float(input("Enter the principle amount:"))
+time1=int(input("Enter the time(years):"))
+rate1=float(input("Enter the rate:"))
+simple_interest1=(principle1*time1*rate1)/100
+print("The simple interest is:",simple_interest1)
