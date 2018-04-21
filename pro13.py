09a7c0867ec43ac87f3a10a326697de9f28c4a60
Nivethika7623 <36299119+Nivethika7623@users.noreply.github.com>
2018-04-21 11:54:52 +0530

    pro13.py

---
+++ pro13.py
@@ -0,0 +1,14 @@
+m=int(input())
+n=int(input())
+l=[]
+l1=[]
+for i in range(m):
+    a=int(input())
+    l.append(a)
+for j in range(n):
+    c=0
+    u=int(input())
+    v=int(input())
+    for i in range(u,v):
+        l1.append(l)
+    print(min(l))
